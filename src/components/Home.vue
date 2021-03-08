4bf173b930ff99e2f177919c7237a5724ae38b7f
ranianadiva <ranianadiva11@gmail.com>
2021-03-07 20:37:10 -0800

    modul 5 vue routing

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Home Page</h1>
+        <p>This is Home Page</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
